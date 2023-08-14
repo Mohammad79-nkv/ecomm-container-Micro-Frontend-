fca2fbbe980e70ceb3b0f3b5c487c9be8a8ba1d4
mohammad <mohammadnv79@gmail.com>
2023-08-14 13:24:44 +0330

    init projects

---
+++ src/bootstrap.js
@@ -0,0 +1,18 @@
+import 'products/productsIndex';
+import 'cart/cartIndex'
+
+// // console.log('Container!');
+
+// let x = true;
+
+// console.log("hello");
+
+// setTimeout(() => {
+//   x = false;
+// }, 0);
+
+// while (x === true) {
+//   console.log(`(ツ)`);
+// }
+
+// console.log("bye");
